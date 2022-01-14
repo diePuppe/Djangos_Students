220a52a65ac2b08f5cd83580ca6273d2586c5a67
diePuppe <95628567+diePuppe@users.noreply.github.com>
2022-01-14 12:40:46 +0100

    adding some comments for understanding and questioning adding logout and login bug fixed

--- website/blog/migrations/0001_initial.py
+++ website/blog/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Eintrag',
+            name='Eintrag',   #django migrates a database in sql ??? functionwise unclear :/
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('message', models.CharField(max_length=250)),
--- website/web/urls.py
+++ website/web/urls.py
@@ -3,7 +3,8 @@ from .import views
 
 app_name = 'web'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  #the name is a shortcut to the actual path
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
